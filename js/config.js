3869e3881c85f953bbad0217d1bd38c08350c87b
Chris Knowles <knowles.chris.d@gmail.com>
2019-01-23 20:28:09 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-  space_id: 'bsux5spekp1p',
+  space_id: '<space-id>',
   environment_id: 'master',
   content_type:'article',
   /* if you are using OAuth */
-  client_id: 'cd98057e0b5323e06758710994ac45c94b881c90f196c56af7a4a0d46d689731',
-  redirect_url: 'https://localhost:4443/content-calendar/',
+  client_id: '<client-id>',
+  redirect_url: '<redirect-url>',
   /* if you are not using OAuth */
-  token: 'CFPAT-cbd36b70ae8da8df5ff26fb139f4520bd2de707609dde1cd7e4140ffe0bfb831'
+  token: '<CMA-token>'
 }
